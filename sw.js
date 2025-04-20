771599aa35954b85be14472c7d43744f127cd2cc
Sebastian Lobo <salobo@uc.cl>
2025-04-20 00:05:55 -0400

    update(sw): new event listener

--- sw.js
+++ sw.js
@@ -4,19 +4,47 @@ const filesToCache = [
   "/",
   "manifest.json",
   "index.html",
+  "index.js",
+  "style.css",
   "/images/icon512_rounded.png",
+  "/images/application-16.png"
 ];
 
+// Instalar el service worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+      return cache.addAll(filesToCache)
+      .then(() => self.skipWaiting())
     }).catch((error) => {
       console.error("Error caching files:", error);
     })
   );
 });
 
+// Funcione sin conexión
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (cacheWhitelist.indexOf(name) === -1) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+    .then(() => self.clients.claim())
+    .catch((error) => {
+      console.error("Error during service worker activation:", error);
+    })
+  );
+})
+
+// Interceptar las solicitudes de red
+// y servir los archivos desde la caché si están disponibles
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
